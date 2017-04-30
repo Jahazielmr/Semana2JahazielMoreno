9e6a9e65690e8cf592fda983567efe2ba9696590
Jahazielmr <Jahazielmr97@gmail.com>
2017-04-30 09:13:49 -0600

    Ejercicios Hechos en clase semana 2

---
+++ Estructurascontrol.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+void ejemplo();
+void comparar(int,int);
+
+int main(){
+
+  cout<<"Hola Mundo"<<endl;
+  int salir =1;
+  while(salir ==1){
+  int arreglo[5];
+   
+  for(int i = 0; i < 5; i++){
+    cout<<"Escriba un numero: ";
+    cin>>arreglo[i];
+  }
+
+  
+  cout<<endl<<"Los valores leidos son: "<<endl;
+  for(int i=0;i<5;i++){
+    cout<<arreglo[i]<<endl;
+  }
+  cout<<endl<<"Ingrese 1 para continuar,0 par salir: ";
+  cin>>salir;
+  }
+  ejemplo();
+  int a,b;
+  cout<<"Ingrese el numero a ";
+  cin>>a;
+  cout<<"Ingrese el numero b ";
+  cin>>b;
+  comparar(a,b);
+return 0;
+}
+
+void comparar(int a,int b){
+  if(a>b){
+    cout<<a<<" es mayor que "<<b<<endl;
+    }else if(a==b)
+	cout<<a<<" es igual a "<<b<<endl;
+    else{
+      cout<<b<<" es mayor que "<<a<<endl;
+    }
+  }
+
+void ejemplo(){
+  cout<<"Ejemplo Funcion"<<endl;
+}
+
+
---
+++ Funciones.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+int menu();
+
+int factorial(int num);
+
+int main(){
+
+  bool salir=false;
+  while(!salir){
+    switch(menu()){
+      case 1:{
+        int num1,num2;
+	cout<<"Escriba un numero: ";
+        cin>>num1;
+        cout<<"Escriba un numero: ";
+	cin>>num2;
+  	cout<<"El resultado es: "<<num1 * num2<<endl;
+
+        break;
+      }
+     case 2:{
+       int num3,num4;
+       cout<<"Escriba un numero: ";
+       cin>>num3;
+       cout<<"Escriba un numero: ";
+       cin>>num4;
+       cout<<"El resultado es: "<<num3 + num4<<endl;
+       break;
+     }
+     case 3:{
+       int num5;
+       cout<<"Ingrese un numero para calcular factorial: ";
+       cin>>num5;
+       cout<<"El factorial de "<< num5 <<" es: "<< factorial(num5)<<endl;
+       break;
+     }
+     case 4:
+	salir=true;
+     break;
+
+    }
+  }
+
+  return 0;
+}
+
+int menu(){
+  int opcion;
+  bool valido=false;
+  do{
+
+    cout<<"-----MENU-----"<<endl
+    <<"1.- Multiplicar dos numeros"<<endl
+    <<"2.- Sumar dos numeros"<<endl
+    <<"3.- Factorial"<<endl
+    <<"4.- exit"<<endl;
+
+  cout<<"Ingrese una opcion: ";
+  cin>>opcion;
+
+  if(opcion>0 && opcion < 5)
+    valido=true;
+  else{
+    cout<<"Opcion no disponible"<<endl;
+  }
+
+  }while(!valido);
+
+  return opcion;
+}
+
+int factorial(int num){
+  if(num<0) return 0;
+  else if(num>1)return num*factorial(num-1);
+  return 1;
+}
